36128f297359952104cba9de3fb3fada11b65efe
sandro gagoshidze <49387424+sannyybg@users.noreply.github.com>
2020-06-15 15:42:14 +0400

    Add files via upload

---
+++ Models/Album.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace musicshop.Models
+{
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+
+        public double Price { get; set; }
+
+        public string AlbumUrl { get; set; }
+
+        public int ReleaseYear { get; set; }
+        
+        public Artist Artist { get; set; }
+
+        public Genre Genre { get; set; }
+
+    }
+}
